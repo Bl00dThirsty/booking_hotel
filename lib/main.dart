bb042f88e2f57e1158df7d9d844526203ebcd249
Bl00dThirsty <johndavisambassa@gmail.com>
2024-05-22 18:16:43 +0200

    Images

--- lib/main.dart
+++ lib/main.dart
@@ -221,11 +221,154 @@ class SearchSection extends StatelessWidget {
 }
 
 class HotelSection extends StatelessWidget {
+  final List hotelList = [
+    {
+      'title': 'Grand Royl Hotel',
+      'place': 'Boulevard du 20 Mai, Yaoundé',
+      'distance': 2,
+      'review': 37,
+      'picture': 'images/hotel_1.png',
+      'price': '80',
+    },
+    {
+      'title': 'United Hotel',
+      'place': 'Bastos, Yaoundé',
+      'distance': 2,
+      'review': 37,
+      'picture': 'images/hotel_2.png',
+      'price': '80',
+    },
+    {
+      'title': 'Palce 26',
+      'place': 'Warda, Yaoundé',
+      'distance': 2,
+      'review': 37,
+      'picture': 'images/hotel_3.png',
+      'price': '80',
+    },
+    {
+      'title': 'Palace 23',
+      'place': 'Boulevard du 20 Mai, Yaoundé',
+      'distance': 2,
+      'review': 87,
+      'picture': 'images/hotel_4.png',
+      'price': '80',
+    }
+    
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 2000,
+      padding: EdgeInsets.all(10),
       color: Colors.white,
+      child: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('550 Hotels',
+                  style: GoogleFonts.nunito(
+                    color: Colors.black,
+                    fontSize: 15,
+                  ),
+                  ),
+                  
+                  Row(
+                    children: [
+                      Text('Filtres',
+                      style: GoogleFonts.nunito(
+                        color: Colors.black,
+                        fontSize: 15,
+                      ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.filter_list_outlined,
+                          color: d_green,
+                          size: 25,
+                          ),
+                          onPressed: null,
+                        )
+                    ],
+                  )
+              ],
+            ),
+          ),
+          Column(
+            children: 
+            hotelList.map((hotel){
+              return HotelCard(hotel);}).toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+class HotelCard extends StatelessWidget {
+  final Map hotelData;
+  HotelCard(this.hotelData);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      height: 230,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(15),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade200,
+            spreadRadius: 4,
+            blurRadius: 6,
+            offset: Offset(0, 3),
+          )
+        ]
+      ),
+
+
+//======================  Image  ==================
+      child: Column(
+        children: [
+          Container(
+            height: 140,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+              image: DecorationImage(image: AssetImage(hotelData['picture'],
+              ),
+              fit: BoxFit.cover,
+              ),
+            ),
+            
+            child: Stack( children: [Positioned(
+              top: 5,
+              right: -15,
+              child: MaterialButton(
+                color: Colors.white,
+                shape: CircleBorder(),
+              onPressed: (){},
+              child: Icon(
+              Icons.favorite_outline_rounded,
+              color: d_green,
+              size: 20,
+            ),))],
+
+            ),
+          )
+        ],
+      ),
+
+
+
     );
   }
 }
